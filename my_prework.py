7f3522d27885de3c6d9fd8049beb6627afb49c53
swhitt1987 <126019018+swhitt1987@users.noreply.github.com>
2023-02-21 15:34:34 -0500

    Add files via upload

--- my_prework.py
+++ my_prework.py
@@ -1,45 +1,45 @@
-# #  Question 1
-# #  Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
+# Question 1
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
 
-# def hello_name(user_name):
-#     print("hello_" + user_name.upper() +"!")
+def hello_name(user_name):
+    print("hello_" + user_name.upper() +"!")
 
-# hello_name("USERNAME")
+hello_name("USERNAME")
 
-# #  Question 2
-# #  Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
+# Question 2
+# Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
 
-# def first_odds():
-#     for n in range(1, 100):
-#         if n % 2 == 1:
-#             print(n)
-# # Need to run first_odds to get the list to return
+def first_odds():
+    for n in range(1, 100):
+        if n % 2 == 1:
+            print(n)
+# Need to run first_odds to get the list to return
 
-# #  Question 3
-# #  Please write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.
+# Question 3
+# Please write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.
 
-# def max_num_in_list(a_list):
-#     max_number = a_list[0]
-#     for number in a_list:
-#         if number > max_number:
-#             max_number = number
-#     return max_number
-# a_list = [5, 7, 12, 4, 8]
-# print(max_num_in_list(a_list))
+def max_num_in_list(a_list):
+    max_number = a_list[0]
+    for number in a_list:
+        if number > max_number:
+            max_number = number
+    return max_number
+a_list = [5, 7, 12, 4, 8]
+print(max_num_in_list(a_list))
 
-# # Question 4
-# # Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, 
-# # unless it is also divisible by 400. The return should be boolean Type (true/false).
+# Question 4
+# Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, 
+# unless it is also divisible by 400. The return should be boolean Type (true/false).
 
-# def is_leap_year(a_year):
-#     if a_year % 400 == 0:
-#         return True
-#     elif a_year % 100 == 0:
-#         return False
-#     elif a_year % 4 == 0:
-#         return True
-#     else:
-#         return False
+def is_leap_year(a_year):
+    if a_year % 400 == 0:
+        return True
+    elif a_year % 100 == 0:
+        return False
+    elif a_year % 4 == 0:
+        return True
+    else:
+        return False
 
 # Question 5
 # Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, 
@@ -52,5 +52,3 @@ def is_consecutive(a_list):
             return False
     else: 
         return True
-a_list = [1, 2, 3, 4, 5]
-print(is_consecutive(a_list))
